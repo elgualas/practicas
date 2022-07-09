8ed528293cb845903b31046d464ccc6042a308b9
unknown <drgualas@gmail.com>
2022-07-09 00:14:41 -0500

    ejercicios

--- depp.py
+++ depp.py
@@ -1,5 +1,17 @@
 import numpy as np
+"""
+data1=np.zeros((2,10), dtype=int, order='C')
+data1[1,4]=1
+print(data1)
 
-data1=np.zeros((1,10))
-data1[0,4]=1
+data1=np.arange(10,49,1)
+print(data1)
+
+data1=np.zeros((4,4))
+print(data1.dtype.name)
+"""
+data1=np.arange(64).reshape(4,4,4)
+print(data1)
+data1=data1.transpose(2,0,1)
+print('------------------------')
 print(data1)
